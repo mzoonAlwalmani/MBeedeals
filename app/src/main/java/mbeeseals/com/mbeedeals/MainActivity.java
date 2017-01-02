c7a2faf60addefee856d0a3495fd1c84b4279cf7
Bushra-awwad <bushra.s.awwad@gmail.com>
2017-01-02 17:23:11 +0300

    bushra

--- app/src/main/java/mbeeseals/com/mbeedeals/MainActivity.java
+++ app/src/main/java/mbeeseals/com/mbeedeals/MainActivity.java
@@ -1,11 +1,19 @@
 package mbeeseals.com.mbeedeals;
 
+import android.app.VoiceInteractor;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ncapdevi.fragnav.FragNavController;
 import com.roughike.bottombar.BottomBar;
@@ -21,92 +29,114 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 
+    private BottomBar mBottomBar;
+    private FragNavController fragNavController;
 
-        private BottomBar mBottomBar;
-        private FragNavController fragNavController;
+    //indices to fragments
+    private final int TAB_FIRST = FragNavController.TAB1;
+    private final int TAB_SECOND = FragNavController.TAB2;
+    private final int TAB_THIRD = FragNavController.TAB3;
+    private final int TAB_FOURTH = FragNavController.TAB4;
+    private final int TAB_FIFTH = FragNavController.TAB5;
 
-        //indices to fragments
-        private final int TAB_FIRST = FragNavController.TAB1;
-        private final int TAB_SECOND = FragNavController.TAB2;
-        private final int TAB_THIRD = FragNavController.TAB3;
-        private final int TAB_FOURTH = FragNavController.TAB4;
-        private final int TAB_FIFTH = FragNavController.TAB5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Thread.setDefaultUncaughtExceptionHandler(new Catcho.Builder(this).recipients("bushraawwad93@gmail.com").build());
+        setContentView(R.layout.activity_main);
 
+        Toolbar title_bar = (Toolbar) findViewById(R.id.title_bar);
+        setSupportActionBar(title_bar);
 
-        @Override
-        protected void onCreate (Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            Thread.setDefaultUncaughtExceptionHandler(new Catcho.Builder(this).recipients("bushraawwad93@gmail.com").build());
-            setContentView(R.layout.activity_main);
-
-            Toolbar title_bar = (Toolbar) findViewById(R.id.title_bar);
-            setSupportActionBar(title_bar);
-
-          final  TextView title = (TextView) findViewById(R.id.mytext);
-            //FragNav
-            //list of fragments
-            List<Fragment> fragments = new ArrayList<>(5);
-
-            //add fragments to list
-            fragments.add(SearchFragment.newInstance(0));
-            fragments.add(notifacationFragment.newInstance(0));
-            fragments.add(ThingFragment.newInstance(0));
-            fragments.add(requestFragment.newInstance(0));
-            fragments.add(meetupFragment.newInstance(0));
+        final TextView title = (TextView) findViewById(R.id.mytext);
+        final ImageButton menu = (ImageButton) findViewById(R.id.menu);
 
 
-            //link fragments to container
-            fragNavController = new FragNavController(getSupportFragmentManager(), R.id.container, fragments);
-            //End of FragNav
 
-            //BottomBar menu
-            mBottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        //FragNav
+        //list of fragments
+        List<Fragment> fragments = new ArrayList<>(5);
 
-            mBottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-                @Override
-                public void onTabSelected(@IdRes int tabId) {
-                    //switch between tabs
-                    switch (tabId) {
-                        case R.id.itemOne:
-                            fragNavController.switchTab(TAB_FIRST);
-                            title.setText(R.string.search);
-                            break;
-                        case R.id.itemTow:
-                            fragNavController.switchTab(TAB_SECOND);
-                            title.setText(R.string.notification);
-                            break;
-                        case R.id.itemThree:
-                            fragNavController.switchTab(TAB_THIRD);
-                            title.setText(R.string.ads);
-                            break;
-                        case R.id.itemFour:
-                            fragNavController.switchTab(TAB_FOURTH);
-                            title.setText(R.string.requests);
-                            break;
-                        case R.id.itemFive:
-                            fragNavController.switchTab(TAB_FIFTH);
-                            title.setText(R.string.meeting);
-                            break;
-                    }
+        //add fragments to list
+        fragments.add(SearchFragment.newInstance(0));
+        fragments.add(notifacationFragment.newInstance(0));
+        fragments.add(ThingFragment.newInstance(0));
+        fragments.add(requestFragment.newInstance(0));
+        fragments.add(meetupFragment.newInstance(0));
+
+
+        //link fragments to container
+        fragNavController = new FragNavController(getSupportFragmentManager(), R.id.container, fragments);
+        //End of FragNav
+
+        //BottomBar menu
+        mBottomBar = (BottomBar) findViewById(R.id.bottomBar);
+
+        mBottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                //switch between tabs
+                switch (tabId) {
+                    case R.id.itemOne:
+                        fragNavController.switchTab(TAB_FIRST);
+                        title.setText(R.string.search);
+                        break;
+                    case R.id.itemTow:
+                        fragNavController.switchTab(TAB_SECOND);
+                        title.setText(R.string.notification);
+                        break;
+                    case R.id.itemThree:
+                        fragNavController.switchTab(TAB_THIRD);
+                        title.setText(R.string.ads);
+                        break;
+                    case R.id.itemFour:
+                        fragNavController.switchTab(TAB_FOURTH);
+                        title.setText(R.string.requests);
+                        break;
+                    case R.id.itemFive:
+                        fragNavController.switchTab(TAB_FIFTH);
+                        title.setText(R.string.meeting);
+                        break;
                 }
-            });
-                mBottomBar.setOnTabReselectListener(new OnTabReselectListener() {
-                    @Override
-                    public void onTabReSelected(@IdRes int tabId) {
-                        fragNavController.clearStack();
-                    }
-                });
+            }
+        });
+        mBottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+            @Override
+            public void onTabReSelected(@IdRes int tabId) {
+                fragNavController.clearStack();
+            }
+        });
 
-                //End of BottomBar menu
-            }
-        @Override
-        public void onBackPressed () {
-            if (fragNavController.getCurrentStack().size() > 1) {
-                fragNavController.pop();
-            } else {
-                super.onBackPressed();
-            }
+        //End of BottomBar menu
+         menu.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+
+                 PopupMenu popup = new PopupMenu(MainActivity.this, menu);
+                 popup.getMenuInflater().inflate(R.menu.menu_side, popup.getMenu());
+
+                 popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                     public boolean onMenuItemClick(MenuItem item) {
+                         Toast.makeText(MainActivity.this,
+                                 "You Clicked : " + item.getTitle(),
+                                 Toast.LENGTH_SHORT).show();
+                         return true;
+                     }
+                 });
+
+                 popup.show();
+             }
+         });}
+    @Override
+    public void onBackPressed() {
+        if (fragNavController.getCurrentStack().size() > 1) {
+            fragNavController.pop();
+        } else {
+            super.onBackPressed();
         }
-
     }
 
+
+
+
+}
